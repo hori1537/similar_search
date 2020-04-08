01f9ddc7b3f5cadd9911214a56d8453369730ddd
hori1537 <yuki.horie@cgco.co.jp>
2020-04-08 13:38:04 +0900

    Add files via upload

--- src/similar_search.py
+++ src/similar_search.py
@@ -1,37 +1,41 @@
 
+import warnings
+warnings.resetwarnings()
+warnings.simplefilter('ignore', FutureWarning)
 
 
 import glob
-import numpy as np
-
-import keras
-from keras.models import Model
-from keras.layers import Input, Dense
-from keras.applications.vgg19 import VGG19, preprocess_input
-from keras.applications.inception_v3 import InceptionV3
-from keras.preprocessing import image
-
-import nmslib
-
+from pathlib import Path #pathlibはオブジェクトで扱う
 import tkinter
 import tkinter.filedialog
 from tkinter import ttk,N,E,S,W,font
 
-from pathlib import Path
+#Apache License Version 2.0
+#https://github.com/nmslib/nmslib/blob/master/README.md
+import nmslib
 
+#https://numpy.org/license.html
+import numpy as np
 
-current_path = Path.cwd()
-program_path = Path(__file__).parent.resolve()
-parent_path = program_path.parent.resolve()
+#
+#current_path = Path.cwd()
+program_path = Path(__file__).parent.resolve() #.parent.resolve()：親ディレクトリを取得後、絶対パスに変換 プログラムがおいてある場所
+parent_path = program_path.parent.resolve()    #similarsearchの場所
 
 data_path           = parent_path / 'data'
 data_processed_path = data_path / 'processed'
 
 # refer https://qiita.com/wasnot/items/20c4f30a529ae3ed5f52
+# refer https://qiita.com/K-jun/items/cab923d49a939a8486fc
+
+
 
 def main():
     print("データベースを選択してください")
     print("サブディレクトリ内の画像もすべて検索対象となります")
+
+    tk = tkinter.Tk() 
+    tk.withdraw()     
 
     data_folder_path = tkinter.filedialog.askdirectory(initialdir = data_processed_path,
                         title = 'choose data folder')
@@ -40,9 +44,23 @@ def main():
     test_img_path = tkinter.filedialog.askopenfilename(initialdir = data_processed_path,
                         title = 'choose test image', filetypes = [('image file', '*.jpeg;*jpg;*png')])
 
+    #License
+    #The MIT License
+    import keras
+    from keras.models import Model
+    from keras.layers import Input, Dense
+    from keras.applications.inception_v3 import InceptionV3
+    from keras.preprocessing import image
+    from keras.applications.vgg19 import preprocess_input
+
+    #License
+    #These weights are ported from the ones released by VGG at Oxford under the Creative Commons Attribution License.
+    #https://keras.io/applications/
+    from keras.applications.vgg19 import VGG19, preprocess_input
+
     base_model = VGG19(weights="imagenet")
     #base_model = InceptionV3(weights="imagenet")
-    base_model.summary()
+    #base_model.summary()
     model = Model(inputs=base_model.input, outputs=base_model.get_layer("fc2").output)
 
     test_img = image.load_img(test_img_path, target_size=(224, 224))
@@ -51,13 +69,30 @@ def main():
     x = preprocess_input(x)
     test_fc2_features = model.predict(x)
 
-    png_list  = glob.glob(data_folder_path + "/**/*.png", recursive=True)
+
+    png_list  = glob.glob(data_folder_path + "/**/*.png", recursive=True)   #/**/且つrecursiveで現在のフォルダ以下にフォルダがあっても読み込んでくれる
     jpeg_list = glob.glob(data_folder_path + "/**/*.jpeg", recursive=True)
     jpg_list  = glob.glob(data_folder_path + "/**/*.jpg", recursive=True)
     image_list = png_list + jpeg_list + jpg_list
 
     fc2_list = []
-    for image_path in image_list:
+    print("画像数は" , len(image_list), "です。")
+
+
+    print("探索を開始します。中止する場合はCtrl+Cを押してください")
+    import time
+    start_time = time.time()
+
+    warnings.resetwarnings()
+    warnings.simplefilter('ignore', UserWarning)
+    warnings.simplefilter('ignore', FutureWarning)
+
+    for i, image_path in enumerate(image_list):
+        if  (i == 10) or (i != 0 and i % 100 == 0)  :
+            remained_num =  len(image_list) - i
+            elapsed_time = time.time()
+            remained_time = (elapsed_time-start_time)/i *  remained_num
+            print(i, "件完了しました。　残り", remained_num , "件、" , round(remained_time) ,"秒くらいかかります。")
         img = image.load_img(image_path, target_size=(224, 224))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
@@ -65,6 +100,7 @@ def main():
         fc2_features = model.predict(x)
         fc2_list.append(fc2_features[0])
 
+    warnings.resetwarnings()
     #print(fc2_list)
 
     # Annoy同様にデータを入れてbuildする。Numpy配列で入れられる。
@@ -73,24 +109,25 @@ def main():
     index.createIndex({'post': 2}, print_progress=True)
 
     # 基本的にAnnoy同様に一件ずつ検索して、返却される。
-    ids, distances = index.knnQuery(test_fc2_features, k=len(image_list))
+    ids, distances = index.knnQuery(test_fc2_features[0], k=len(image_list))
     result = [image_list[i] for i in ids]
 
     print(ids)
     print(distances)
     print(result)
 
-    print("選択した画像は " , test_img_path " です")
+    print("選択した画像は " , test_img_path, " です")
 
     print("選択した画像に似ている順に表示します")
     for i, id in enumerate(ids):
         print(image_list[id], " : 距離： ", distances[i])
 
-    print("選択した画像は " , test_img_path " です")
+    print("選択した画像は " , test_img_path, " です")
 
     #index.save(model_name)
     import time
-    time.sleep(3000)
+    print("30分後に画面を閉じます")
+    time.sleep(1800)
 
 
 if __name__ == "__main__" :
